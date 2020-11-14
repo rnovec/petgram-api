df1ca804abde239bf14421ed5a90e16c43849789
Raul Novelo <rnovelo.cruz98@gmail.com>
2020-11-14 00:44:14 -0600

    Filter post by user

--- users/views.py
+++ users/views.py
@@ -4,6 +4,8 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.parsers import MultiPartParser, FormParser
+from petgram.models import Post
+from petgram.serializers import PostSerializer
 from .models import User
 from .serializers import UserSerializer
 
@@ -17,3 +19,16 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     parser_classes = (MultiPartParser, FormParser)
+
+    @action(detail=True, methods=['get'])
+    def posts(self, request, pk=None):
+
+        user_posts = Post.objects.filter(user=pk).order_by('-created')
+
+        page = self.paginate_queryset(user_posts)
+        if page is not None:
+            serializer = PostSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = PostSerializer(user_posts, many=True)
+        return Response(serializer.data)
